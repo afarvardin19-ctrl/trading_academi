222ba9849f5dde2a0b1763baedb134b1f93feed0
your_username <your_email@example.com>
2026-08-28 01:40:14 +0330

    رفع متد ثبت‌نام

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, request, session, redirect, url_for
+from flask import Flask, render_template_string, request, redirect, url_for
 import sqlite3
 import random
 import string
